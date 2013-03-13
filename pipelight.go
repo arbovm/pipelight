8781311dd7af8d08804c8152d33fa12b4b6bf750
Arbo von Monkiewitsch <arbo@soundcloud.com>
2013-03-13 09:41:18 +0100

    Refactored

--- pipelight.go
+++ pipelight.go
@@ -1,42 +1,38 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"github.com/arbovm/highlighter"
-	"bufio"
-	"os"
-	"io/ioutil"
 	"io"
-	"bytes"
+	"io/ioutil"
+	"os"
 )
+
+func readLine(reader *bufio.Reader) (string, error) {
+	return reader.ReadString('\n')
+}
 
 func main() {
 	filename := fmt.Sprintf("/tmp/pipelight-%v", os.Getppid())
-	file, readError := os.Open(filename)
-	stdinReader     := bufio.NewReader(os.Stdin)
-	reader          := bufio.NewReader(file)
+	file, oldInErr := os.Open(filename)
+	defer file.Close()
+	oldIn := bufio.NewReader(file)
+	in := bufio.NewReader(os.Stdin)
 	var oldLine string
 	var lines bytes.Buffer
-	for {
-		line,  err := stdinReader.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-		if readError == nil {
-			oldLine, readError = reader.ReadString('\n')
-		}
 
-		if readError == nil {
+	for line, err := readLine(in); err != io.EOF; line, err = readLine(in) {
+		lines.WriteString(line)
+		if oldInErr == nil {
+			oldLine, oldInErr = readLine(oldIn)
+		}
+		if oldInErr == nil {
 			fmt.Print(highlighter.DiffLines(oldLine, line))
 		} else {
 			fmt.Print(line)
 		}
-		lines.WriteString(line)
-	}
-	if readError == nil {
-		file.Close()
 	}
 	ioutil.WriteFile(filename, lines.Bytes(), 0644)
-	
 }
-
